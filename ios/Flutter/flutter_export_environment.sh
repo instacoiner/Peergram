5968359a99e420bbf2809a148570532fab033281
Zehady Abdullah Khan <brishtiteveja@gmail.com>
2020-05-15 13:20:53 -0400

    Peergram initial commit. App runs on Google Pixel 2xl. We have not tested on other devices yet. Firebase Cloud functions running properly.

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/andy/Documents/projects/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/andy/Documents/projects/fluttergram"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/andy/Documents/projects/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.2.3"
+export "FLUTTER_BUILD_NUMBER=1"
